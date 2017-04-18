80137a8d6ec50b48eb27dea570b845a0459e4c48
Robert Schulze <robert@dotless.de>
2017-04-18 15:58:01 +0200

    fix spec to work on macOS 10.12
    using a more complex Regexp for matching the scutil output

--- test/acceptance/dns/dns_spec.rb
+++ test/acceptance/dns/dns_spec.rb
@@ -19,11 +19,10 @@ shared_examples 'provider/dns' do |provider, options|
   describe 'installation' do
     it 'creates and removes resolver link' do
       assert_execute('vagrant', 'dns', '--install', '--with-sudo')
-      assert_execute('ls', "/etc/resolver/#{tld}")
+      assert_execute('sudo', 'test', '-f', "/etc/resolver/#{tld}")
 
       assert_execute('vagrant', 'dns', '--uninstall', '--with-sudo')
-      result = execute('ls', "/etc/resolver/#{tld}")
-      expect(result).to_not exit_with(0)
+      assert_execute('sudo', 'test', '!', '-f', "/etc/resolver/#{tld}")
     end
   end
 
@@ -36,7 +35,7 @@ shared_examples 'provider/dns' do |provider, options|
 
     after do
       # Ensure any VMs that survived tests are cleaned up.
-      assert_execute('vagrant', 'destroy', '--force', log: false)
+      execute('vagrant', 'destroy', '--force', log: false)
       assert_execute('vagrant', 'dns', '--uninstall', '--with-sudo')
     end
 
@@ -45,16 +44,17 @@ shared_examples 'provider/dns' do |provider, options|
     end
 
     it 'registered as a resolver' do
-      expected_output = <<-TXT
-  domain   : #{tld}
-  nameserver[0] : 127.0.0.1
-  port     : 5333
-  flags    : Request A records, Request AAAA records
-  reach    : Reachable,Local Address
+      # the output of `scutil` changes it's format over MacOS versions a bit
+      expected_output = Regexp.new(<<-TXT.gsub(/^\s*/, ''), Regexp::MULTILINE)
+        \\s*domain\\s*: #{tld}
+        \\s*nameserver\\[0\\]\\s*: 127.0.0.1
+        \\s*port\\s*: 5333
+        \\s*flags\\s*: Request A records, Request AAAA records
+        \\s*reach\\s*: Reachable,\\s?Local Address(, Directly Reachable Address)?
       TXT
 
       result = assert_execute('scutil', '--dns')
-      expect(result.stdout).to include(expected_output)
+      expect(result.stdout).to match(expected_output)
     end
 
     it 'responds to host-names' do
