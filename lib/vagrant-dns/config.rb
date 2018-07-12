db181ccf5c662f582c83c297a15118024580f259
Robert Schulze <robert@dotless.de>
2018-07-12 14:03:51 +0200

    remove unused “ipv4only” configuration

--- lib/vagrant-dns/config.rb
+++ lib/vagrant-dns/config.rb
@@ -3,23 +3,19 @@ require 'logger'
 module VagrantDNS
   class Config < Vagrant.plugin(2, :config)
     class << self
-      attr_accessor :listen, :logger, :ipv4only, :auto_run
+      attr_accessor :listen, :logger, :auto_run
 
       def listen
         @listen ||= [[:udp, "127.0.0.1", 5300]]
       end
 
-      def ipv4only
-        @ipv4only || @ipv4only.nil?
-      end
-      
       def auto_run
         return true if @auto_run.nil?
         @auto_run
       end
     end
 
-    attr_accessor :records, :tlds, :ipv4only, :patterns
+    attr_accessor :records, :tlds, :patterns
 
     def pattern=(pattern)
       self.patterns = pattern
@@ -39,7 +35,6 @@ module VagrantDNS
         :patterns => (patterns ? Array(patterns) : patterns),
         :records => records,
         :tlds => tlds,
-        :ipv4only => ipv4only
       }
     end
   end
